8288caab62d31d0ac76cec30ce5db621136eb713
JuanJoseFamiliaPolanco <familiajuan05@gmail.com>
2024-12-05 14:33:19 -0400

    final

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const logger = require('./logger'); // Importa el logger
 const app = express();
 
-// Configura el servidor para responder con un mensaje simple en /api/message
+let requestCount = 0;
+
+app.use((req, res, next) => {
+  requestCount++;
+  logger.info(`Solicitudes recibidas: ${requestCount}`);
+  next();
+});
+
 app.get('/api/message', (req, res) => {
   res.status(200).json({ message: '¡Hola desde el backend!' });
 });
 
-// Exporta la aplicación para usarla en las pruebas sin iniciar el servidor aquí
 module.exports = app;
 
-// Solo inicia el servidor cuando no se esté corriendo en modo de prueba
 if (require.main === module) {
   app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+    logger.info('Servidor corriendo en http://localhost:3000');
   });
 }
